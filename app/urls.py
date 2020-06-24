91a4b37d664412b5b583848cd20dd56febeb5428
Luiz Araujo <luiz.araujodtna@gmail.com>
2020-06-24 15:50:40 -0300

    config2

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='url_home'),
@@ -7,4 +9,4 @@ urlpatterns = [
     path('new_item/', new_item, name='url_new_item'),
     path('new_item_tag/', new_item_tag, name='url_new_item_tag'),
     path('delete_item/<int:id>', delete_item, name="url_delete_item"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
